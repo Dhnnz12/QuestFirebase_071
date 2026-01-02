25ade97d4f65c3449f54c2c4a8253963871a377a
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-01-02 21:35:06 +0700

    menambahkan pengimplementasian dari interface DestinasiNavigasi yakni DestinasiHome untuk mendefinisikan route home dan title resource yang mengambil dari String nama_app

---
+++ app/src/main/java/com/example/questfirebase_071/ui/theme/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_071.ui.theme.route
+
+import com.example.questfirebase_071.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
