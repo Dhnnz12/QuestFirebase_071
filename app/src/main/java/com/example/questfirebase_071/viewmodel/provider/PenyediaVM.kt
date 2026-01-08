8f42753e7bc7ac2af6f15398e6b14c5ba5e28513
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-01-08 11:44:06 +0700

    menambahkan detailviemodel dan editviewmodel didalam penyediavm

--- app/src/main/java/com/example/questfirebase_071/viewmodel/provider/PenyediaVM.kt
+++ app/src/main/java/com/example/questfirebase_071/viewmodel/provider/PenyediaVM.kt
@@ -3,19 +3,34 @@ package com.example.questfirebase_071.viewmodel.provider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.questfirebase_071.repository.AplikasiDataSiswa
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questfirebase_071.viewmodel.DetailViewModel
+import com.example.questfirebase_071.viewmodel.EditViewModel
 import com.example.questfirebase_071.viewmodel.EntryVM
 import com.example.questfirebase_071.viewmodel.HomeVM
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
-object PenyediaVM{
+
+object PenyediaVM {
     val Factory = viewModelFactory {
         initializer { HomeVM(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryVM(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
